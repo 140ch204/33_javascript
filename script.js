6940313fc1003c121ebc4243b45edb6a26f1ed23
140ch204 <140ch204_github@gadz.org>
2019-11-14 11:53:06 +0100

    V1

---
+++ script.js
@@ -0,0 +1,28 @@
+function script_1() {
+  answer = prompt("Quel est ton prénom ?");
+  console.log('Bonjour monde ${answer}');
+}
+
+function script_2() {
+  console.log("Hello world !");
+}
+
+function script_3() {
+  console.log("Hello world !");
+}
+
+function script_4() {
+  console.log("Hello world !");
+}
+
+function script_5() {
+  console.log("Hello world !");
+}
+
+function script_6() {
+  console.log("Hello world !");
+}
+
+function script_7() {
+  console.log("Hello world !");
+}
