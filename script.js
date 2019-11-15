dd4e090d8905331fdc799c7be5465bbf650a5b82
140ch204 <140ch204_github@gadz.org>
2019-11-15 01:33:20 +0100

    V1

--- script.js
+++ script.js
@@ -9,16 +9,25 @@ function script_1() {
 function script_2() {
   // Un prompt s'affiche avec la question suivante
   num = prompt("De quel nombre veux-tu calculer la factorielle ?")
-  let fact = 1
-  for(let count = 1; count <= num; count++){
-    fact = fact * count
+  if (num < 0) {
+    console.log(`erreur : Nombre négatif`);
   }
-  console.log(`${num}! = ${fact}`)
-  document.getElementById("resultat").innerHTML = (`${num}! = ${fact}`)
+  else if ( num === 0 ){
+    console.log(`${num}! = 1`);
+  }
+  else {  
+    let fact = 1
+    for(let count = 1; count <= num; count++){
+      fact = fact * count
+    }
+    console.log(`${num}! = ${fact}`)
+    document.getElementById("resultat").innerHTML = (`${num}! = ${fact}`)
+  }
 }
 
 
 function script_3() {
+  // Pyramide de Mario
   let etages ="";
   etages = prompt("Combien d'étages veux-tu pour ta super pyramide ?");
   
@@ -29,6 +38,7 @@ function script_3() {
 }
 
 function script_4() {
+  // Petits exos sur les array et hashes
   const entrepreneurs = [
     { first: 'Steve', last: 'Jobs', year: 1955 },
     { first: 'Oprah', last: 'Winfrey', year: 1954 },
@@ -70,13 +80,118 @@ function script_4() {
 }
 
 function script_5() {
-  console.log("Hello world !");
-}
+  // Petits exo pour la bibliothèque !
+  const books = [
+    { title: 'Gatsby le magnifique', id: 133712, rented: 39 },
+    { title: 'A la recherche du temps,perdu', id: 237634, rented: 28 },
+    { title: 'Orgueil & Préjugés', id: 873495, rented: 67 },
+    { title: 'Les frères Karamazov', id: 450911, rented: 55 },
+    { title: 'Dans les forêts de Sibérie', id: 8376365, rented: 15 },
+    { title: 'Pourquoi j\'ai mangé mon père', id: 450911, rented: 45 },
+    { title: 'Et on tuera tous les affreux', id: 67565, rented: 36 },
+    { title: 'Le meilleur des mondes', id: 88847, rented: 58 },
+    { title: 'La disparition', id: 364445, rented: 33 },
+    { title: 'La lune seule le sait', id: 63541, rented: 43 },
+    { title: 'Voyage au centre de la Terre', id: 4656388, rented: 38 },
+    { title: 'Guerre et Paix', id: 748147, rented: 19 }
+  ];
+
+
+  console.log("Est-ce que tous les livres ont été au moins empruntés une fois ?");
+    books.sort(function (a, b) {
+      return a.rented - b.rented;
+    })
+    console.log((books[0].rented>0)? true : false);
+
+  console.log("Quel est livre le plus emprunté ?");
+    books.sort(function (a, b) {
+      return b.rented - a.rented;
+    })
+    console.log(books[0].title + " avec "+ books[0].rented + " emprunts" );
+
+  console.log("Quel est le livre le moins emprunté ?");
+  books.sort(function (a, b) {
+    return a.rented - b.rented;
+  })
+  console.log(books[0].title + " avec "+ books[0].rented + " emprunts" );
+
+  console.log("Trouve le livre avec l'ID: 873495 ;");
+  console.log(`${books.filter(function(book){return book.id === 873495})[0].title}`);
+
+
+  console.log("Supprime le livre avec l'ID: 133712 ;");
+  console.log(books2 = books.filter(function(book){return book.id !== 133712}));
+
+  console.log("Trie les livres par ordre alphabétique (sans celui avec l'ID 133712 car il est supprimé).");
+  console.log(books2.sort((a,b) => (a.title > b.title) ? 1 : ((b.title > a.title) ? -1 : 0)));
+};
 
 function script_6() {
-  console.log("Hello world !");
+  // Converti l'ARN en ADN
+
+  const arn1 = "CCGUCGUUGCGCUACAGC";
+  const arn2 = "CCUCGCCGGUACUUCUCG";    
+
+  function codon_to_am (codon) {
+
+    let am = "";
+    if ((codon == "UCU") || (codon == "UCC") || (codon == "UCA") || (codon == "UCG") || (codon == "AGU") || (codon == "AGC")) {
+      am = "Sérine";} 
+    else if ((codon == "CCU") || (codon == "CCC") || (codon == "CCA") || (codon == "CCG")) {
+      am = "Proline";} 
+    else if (codon == "UUA" || codon == "UUG") {
+      am = "Leucine";} 
+    else if (codon == "UUU" || codon == "UUC") {
+      am = "Phénylalanine";} 
+    else if (codon == "CGU" || codon == "CGC" || codon == "CGA" || codon == "CGG" || codon == "AGA" || codon == "AGG") {
+      am = "Arginine";} 
+    else if (codon == "UAU" || codon == "UAC") {
+      am = "Tyrosine";}
+    return am
+  };
+
+  function become (arn) {
+    let adn = ""
+    for (let i = 0; i<(arn.length)/3; i++){
+      adn = adn + " - " + codon_to_am(arn.slice(0+3*i,3+3*i))
+      ;
+    };
+    return(adn)
+  };
+
+  console.log(` ${arn1} va devenir ${become(arn1)}`);
+  console.log(` ${arn2} va devenir ${become(arn2)}`);
 }
 
 function script_7() {
-  console.log("Hello world !");
+  // Adobot party !
+
+  answer = prompt("Bonjour je suis botado. Comment puis-je ne pas t'aider ?");
+
+  let next_one = true
+
+  if (answer.length === 0 && next_one === true ){ 
+    console.log("t'es en PLS ?"); 
+    next_one = false;
+    };
+  
+  if (answer.slice(-1)==="?" ){ 
+    console.log("Ouais Ouais..."); 
+    next_one = false;
+    };
+
+  if (answer === answer.toUpperCase() && next_one === true ){ 
+    console.log("Pwa, calme-toi..."); 
+    next_one = false;
+    };
+
+  if (answer.toUpperCase().includes("FORNITE") && next_one === true ){ 
+    console.log("on s' fait une partie soum-soum ?"); 
+    next_one = false;
+    };
+
+  if ( next_one === true ){ 
+    console.log("balek.");; 
+    };
+
 }
